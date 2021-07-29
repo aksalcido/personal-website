df5bb9ea18b4805c556d6a7c369d7f3ebcaadd87
aksalcido <aksalcid@uci.edu>
2021-07-29 04:54:40 -0700

    Styling, More Front Page Content

--- public/scripts/front_page.js
+++ public/scripts/front_page.js
@@ -1,7 +1,9 @@
 const navbar = document.getElementsByTagName("nav")[0];
 const navbarListItems = document.querySelectorAll(".navbar-item a")
 
+
 const contactTab = document.getElementById("nav-contact");
+const emailIcon = document.getElementById("email-icon");
 
 window.onscroll = function() {
     let scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
@@ -29,4 +31,24 @@ contactTab.addEventListener("click", () => {
     // Compatability of calculating height of different browsers
     let height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
     window.scrollTo(0, height);
+});
+
+emailIcon.addEventListener("click", () => {
+    /* Get the text field */
+    let tempInput = document.createElement('input');
+    tempInput.value = "armandosalcido024@gmail.com";
+    
+    document.body.appendChild(tempInput);
+
+    /* Select the text field */
+    tempInput.select();
+    tempInput.setSelectionRange(0, 99999); /* For mobile devices */
+    
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+    
+    tempInput.remove();
+
+    /* Alert the copied text */
+    alert("Copied Email");
 });
