de0fc45d3e633b6b5c69164e593733dd4eead4b1
aksalcido <aksalcid@uci.edu>
2021-07-30 03:54:01 -0700

    Removed Node.js to create Static Website

--- public/scripts/contact.js
+++
@@ -1,6 +0,0 @@
-let body = document.body;
-let html = document.documentElement;
-
-// Compatability of calculating height of different browsers
-let height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
-window.scrollTo(0, height);
